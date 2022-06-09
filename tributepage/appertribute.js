7619edefba36dfd716ecd0f4d8c694b422afe4aa
alber <aicarq@hotmail.com>
2022-06-09 16:41:47 -0500

    pictures aded to NEW folder kobes-pictures, html added header and list items from current ul

--- tributepage/appertribute.js
+++ tributepage/appertribute.js
@@ -12,3 +12,12 @@ Your #image should use max-width and height properties to resize responsively, r
 
 
 */
+const kobesQuotes = [
+    'It’s the one thing you can control. You are responsible for how people remember you—or don’t. So don’t take it lightly.',
+    'I can’t relate to lazy people. We don’t speak the same language. I don’t understand you. I don’t want to understand you.',
+    'I’ve played with IVs before, during and after games. I’ve played with a broken hand, a sprained ankle, a torn shoulder, a fractured tooth, a severed lip, and a knee the size of a softball. I don’t miss 15 games because of a toe injury that everybody knows wasn’t that serious in the first place.',
+    'Everything negative – pressure, challenges – is all an opportunity for me to rise.',
+    'I had to organize things. So I created the ‘Black Mamba’. So Kobe has to deal with these issues, all the personal challenges. The Black Mamba steps on the court and does what he does. I’m destroying everybody that steps on the court.',
+    'These young guys are playing checkers. I’m out there playing chess.',
+    'I’ve shot too much from the time I was 8 years old. But ‘too much’ is a matter of perspective. Some people thought Mozart had too many notes in his compositions. Let me put it this way: I entertain people who say I shoot too much. I find it very interesting. Going back to Mozart, he responded to critics by saying there were neither too many notes or too few. There were as many as necessary.”</li>               <li>“I would go 0-for-30 [from the floor] before I would go 0-for-9. [An] 0-for-9 means you beat yourself, you psyched yourself out of the game. … The only reason is because you’ve just now lost confidence in yourself.'
+]
